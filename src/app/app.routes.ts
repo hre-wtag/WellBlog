d724bfa82fdaa81d0737d762c9297609ab3245f0
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-23 15:00:59 +0600

    added routed page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { HomeComponent } from './features/home/home.component';
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '',title:'Home', component: HomeComponent },
   {
     path: 'login',
+    title: 'Login',
     component: LoginComponent,
   },
   {
     path: 'register',
+    title: 'Register',
     component: RegisterComponent,
   },
   {
