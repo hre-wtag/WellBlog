cb615e3e01c7c11f45b510e1b6d4dfb8075e3121
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-25 16:34:52 +0600

    added email regext

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -2,3 +2,4 @@ export const TITLE: string = 'WellBlog';
 export const HOME_ROUTE: string = '/';
 export const LOGIN_ROUTE: string = '/login';
 export const REGISTER_ROUTE: string = '/register';
+export const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
