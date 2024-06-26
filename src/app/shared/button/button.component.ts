8749c79fe9360338f21b0e42ab6b7e153f003b6e
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-26 15:19:07 +0600

    made a dynamic button component

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() btnType!: string;
+  @Input() classes!: string;
+  @Input() isDisabled!: boolean;
+
+  @Output()
+  clickEvent = new EventEmitter<void>();
+  onClicked(): void {
+    this.clickEvent.emit();
+  }
+}
