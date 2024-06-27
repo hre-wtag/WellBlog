c5abc4acd8a952005eaec072419c42e09ea140b8
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-27 13:42:55 +0600

    removed unused imports

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
   DefaultValueAccessor,
   FormControl,
