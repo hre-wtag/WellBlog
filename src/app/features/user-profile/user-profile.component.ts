0bd0ec925bacb5797a26b6b4d7d71876349310e8
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-30 15:43:55 +0600

    code refactor

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { HeaderComponent } from '../../shared/header/header.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 
