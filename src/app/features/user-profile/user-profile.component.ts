edaa4b925110ad18b649750074a9548266b28008
Helena Aktar Reya <helena.reya@welldev.io>
2024-06-30 16:33:26 +0600

    added add blog page

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { UserInfoComponent } from './user-info/user-info.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [HeaderComponent,UserInfoComponent],
+  imports: [HeaderComponent, UserInfoComponent, AddBlogComponent],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
-
+  clickedAddBlog: boolean = false;
 }
