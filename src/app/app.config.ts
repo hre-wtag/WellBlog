257803dac44ade7b1f7c2d6c78d45bbeb9b40720
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-01 16:58:10 +0600

    code refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
