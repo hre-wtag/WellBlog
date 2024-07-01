d7ee0337611071ed205882e23c5a61cbe984fa13
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-01 13:18:06 +0600

    added base design for  add blogg form

--- src/app/features/user-profile/add-blog/add-blog.component.ts
+++ src/app/features/user-profile/add-blog/add-blog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputComponent } from '../../../shared/input/input.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import {
@@ -7,16 +7,21 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ValidatorsService } from '../../../core/services/validators.service';
 
 @Component({
   selector: 'app-add-blog',
   standalone: true,
-  imports: [InputComponent, ButtonComponent, ReactiveFormsModule],
+  imports: [CommonModule, InputComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './add-blog.component.html',
   styleUrl: './add-blog.component.scss',
 })
 export class AddBlogComponent {
   addBlogForm: FormGroup;
+  errorMsg: string | null = null;
+  uploadedFileName: string | null = null;
+  private validatorService = inject(ValidatorsService);
   constructor() {
     this.addBlogForm = new FormGroup({
       title: new FormControl('', [
@@ -24,18 +29,41 @@ export class AddBlogComponent {
         Validators.minLength(3),
         Validators.maxLength(15),
       ]),
+      tags: new FormControl('', Validators.required),
+      fileUpload: new FormControl('', Validators.required),
+      blogBody: new FormControl('', Validators.required),
     });
   }
   getFormControl = (formGroup: FormGroup, formControlName: string) => {
     return formGroup.get(formControlName) as FormControl;
   };
-  onSubmit(event: Event): void {
+  onSubmit(): void {
     console.log('Submitted');
   }
-  onCancel(event: Event): void {
+  onCancel(): void {
     console.log('Cancelled');
   }
-  handleImageFileChange(event: FileList | null): void {
-    console.log(event, 'image');
+  handleImageFileChange(event: Event): void {
+    const imageFile = (<HTMLInputElement>event.target)?.files;
+    if (imageFile) {
+      console.log(imageFile[0], 'image');
+      this.uploadedFileName = imageFile[0].name;
+    }
+  }
+  // updateErrorMessages(): void {
+  //   // this.errorMsg = null;
+  //   // const usernameControl = this.addBlogForm.get('title');
+  //   // if (usernameControl?.touched && usernameControl?.errors) {
+  //   //   this.errorMsg = this.validatorService.getErrorMessages(
+  //   //     usernameControl.errors
+  //   //   );
+  //   // }
+  // }
+  onTouched(fieldName: string): void {
+    //   const control = this.addBlogForm.get(fieldName);
+    //   if (control) {
+    //     control.markAsTouched();
+    //     // this.updateErrorMessages();
+    //   }
   }
 }
