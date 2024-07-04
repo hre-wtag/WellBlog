c204f22a342f5279b93e1c54cf9168abd37700d6
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-04 18:48:36 +0600

    working on add blog image uploading

--- src/app/features/user-profile/add-blog/add-blog.component.ts
+++ src/app/features/user-profile/add-blog/add-blog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToasterService } from '../../../core/services/toaster.service';
 export interface Tag {
   title: string;
   isChecked: boolean;
@@ -30,6 +31,7 @@ export class AddBlogComponent {
     { title: 'Fiction', isChecked: false },
     { title: 'World Politics', isChecked: false },
   ];
+  private toasterService = inject(ToasterService);
   constructor() {
     this.addBlogForm = new FormGroup({
       title: new FormControl('', [
@@ -50,13 +52,6 @@ export class AddBlogComponent {
   }
   onCancel(): void {
     console.log('Cancelled');
-  }
-  handleImageFileChange(event: Event): void {
-    const imageFile = (<HTMLInputElement>event.target)?.files;
-    if (imageFile) {
-      console.log(imageFile[0], 'image');
-      this.uploadedFileName = imageFile[0].name;
-    }
   }
 
   onTouched(fieldName: string): void {
@@ -80,7 +75,54 @@ export class AddBlogComponent {
   }
 
   checkDropdown(flag: boolean): void {
-    console.log('click dp');
     this.showDropdown = flag;
+  }
+
+  handleImageFileChange(event: Event): void {
+    event.preventDefault();
+    console.log(event, 'event');
+
+    const imageFile = (<HTMLInputElement>event.target)?.files;
+    if (imageFile) {
+      console.log(imageFile[0], 'image');
+      this.uploadedFileName = imageFile[0].name;
+    }
+  }
+  dropHandler(ev: DragEvent) {
+    ev.preventDefault();
+    console.log(ev);
+    if (ev.dataTransfer?.items) {
+      const files = Array.from(ev.dataTransfer.items);
+      files.forEach((item, i) => {
+        if (item.kind === 'file') {
+          const file = item.getAsFile();
+          if (this.validateFileType(file?.type)) {
+            console.log(`… file[${i}].name = ${file?.name}`);
+            console.log(file);
+          } else {
+            console.warn('Invalid file type. Only image files allowed.');
+            this.toasterService.warning(
+              'Invalid!',
+              'Invalid file type. Only image files allowed.'
+            );
+          }
+        }
+      });
+    }
+  }
+  validateFileType(fileType: string | undefined): boolean | null {
+    if (fileType) {
+      const allowedTypes: string[] | undefined = [
+        'image/jpeg',
+        'image/jpg',
+        'image/png',
+      ];
+      return allowedTypes.includes(fileType);
+    }
+    return null;
+  }
+  dragOver(event: Event) {
+    console.log('click dragOver');
+    event.preventDefault();
   }
 }
