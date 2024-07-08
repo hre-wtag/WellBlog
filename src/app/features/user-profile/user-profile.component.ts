7427f8557bd7004490f59a4ed1146906560becf7
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-08 10:40:33 +0600

    added blog card component

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { AddBlogComponent } from './add-blog/add-blog.component';
+import { BlogCardComponent } from '../blog-card/blog-card.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [UserInfoComponent, AddBlogComponent],
+  imports: [UserInfoComponent, AddBlogComponent, BlogCardComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
