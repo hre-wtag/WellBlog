c55384ffac6c85f017912367fcc2323b76f48328
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-11 16:12:02 +0600

    for not found blog redirected to home

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,13 +1,19 @@
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BlogService } from '../../core/services/blog.service';
 import { Blog } from '../../core/interfaces/blog';
 import { map } from 'rxjs';
+import {
+  DEFAULT_PROFILE_PHOTO_SRC,
+  HOME_ROUTE,
+} from '../../core/utils/constants';
+import { CommonModule } from '@angular/common';
+import { PreviousRouteService } from '../../core/services/previous-route.service';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
@@ -15,7 +21,9 @@ export class BlogComponent implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   private blogService = inject(BlogService);
   private destroyRef = inject(DestroyRef);
+  private router = inject(Router);
   blog: Blog | null = null;
+  default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
   ngOnInit(): void {
     const routeSubscription = this.activatedRoute.paramMap.subscribe({
       next: (paramMap) => {
@@ -28,7 +36,9 @@ export class BlogComponent implements OnInit {
           )
           .subscribe((blogs) => {
             this.blog = blogs?.[0] ?? null;
-            console.log(this.blog, 'blog');
+            if (this.blog === null) {
+              this.router.navigate(['']);
+            }
           });
         this.destroyRef.onDestroy(() => blogSubcription.unsubscribe());
       },
