0bb96b7075465dca5a0b53c865af561b99837ec8
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-11 12:45:55 +0600

    got blogs on subscription

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../../core/services/blog.service';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
   imports: [],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrl: './blog.component.scss',
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private blogService = inject(BlogService);
 
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    const routeSubscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        console.log(paramMap.get('id'));
+        this.blogService.blogs$.subscribe((blogs) => {
+          console.log(blogs);
+        });
+      },
+    });
+  }
 }
