e50be78da4038f879edad1d6572f721593361b16
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-11 13:31:39 +0600

    added routes to go to single  blog

--- src/app/features/blog-card/blog-card.component.ts
+++ src/app/features/blog-card/blog-card.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Blog } from '../../core/interfaces/blog';
-import { DEFAULT_PROFILE_PHOTO_SRC } from '../../core/utils/constants';
+import {
+  BLOG_ROUTE,
+  BLOG_ROUTE_WITH_PARAM,
+  DEFAULT_PROFILE_PHOTO_SRC,
+  SLASH,
+} from '../../core/utils/constants';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './blog-card.component.html',
   styleUrl: './blog-card.component.scss',
 })
 export class BlogCardComponent {
   @Input() blog!: Blog;
   default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
+  blog_route = SLASH + BLOG_ROUTE;
 }
--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../../core/services/blog.service';
+import { Blog } from '../../core/interfaces/blog';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-blog',
@@ -12,16 +14,31 @@ import { BlogService } from '../../core/services/blog.service';
 export class BlogComponent implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   private blogService = inject(BlogService);
-
+  private destroyRef = inject(DestroyRef);
+  blog: Blog | null = null;
   ngOnInit(): void {
     console.log(this.activatedRoute);
     const routeSubscription = this.activatedRoute.paramMap.subscribe({
       next: (paramMap) => {
-        console.log(paramMap.get('id'));
-        this.blogService.blogs$.subscribe((blogs) => {
-          console.log(blogs);
-        });
+        const blogID = paramMap.get('id');
+        // this.blogService.blogs$.subscribe((blogs) => {
+        //   this.blog =
+        //     blogs?.find((blog: Blog) => blog.id.toString() === blogID) ?? null;
+        //   console.log(this.blog, 'blog');
+        // });
+        const blogSubcription = this.blogService.blogs$
+          .pipe(
+            map((arr) =>
+              arr?.filter((blog: Blog) => blog.id.toString() === blogID)
+            )
+          )
+          .subscribe((blogs) => {
+            this.blog = blogs?.[0] ?? null;
+            console.log(this.blog, 'blog');
+          });
+        this.destroyRef.onDestroy(() => blogSubcription.unsubscribe());
       },
     });
+    this.destroyRef.onDestroy(() => routeSubscription.unsubscribe());
   }
 }
