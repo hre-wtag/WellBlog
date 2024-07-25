811b2187a3c157ddcb02c95a5db56ed1f3acc9e1
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-25 13:21:59 +0600

    got updated user data

--- src/app/features/user-profile/edit-user/edit-user.component.ts
+++ src/app/features/user-profile/edit-user/edit-user.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -8,6 +8,8 @@ import {
 } from '@angular/forms';
 import { SharedService } from '../../../core/services/shared.service';
 import { DEFAULT_PROFILE_PHOTO_SRC } from '../../../core/utils/constants';
+import { User } from '../../../core/interfaces/user';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-edit-user',
@@ -16,19 +18,34 @@ import { DEFAULT_PROFILE_PHOTO_SRC } from '../../../core/utils/constants';
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.scss',
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
   @Output() formSubmitted = new EventEmitter<string | null>();
   editUserForm: FormGroup;
+  userInfo: User | null = null;
   private sharedService = inject(SharedService);
+  private authService = inject(AuthService);
   uploadedImageName: string | null = null;
   uploadedImage: File | null = null;
   default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
+
   constructor() {
     this.editUserForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
       subtitle: new FormControl(''),
       about: new FormControl(''),
+    });
+  }
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user: User | null) => {
+      this.userInfo = user ?? null;
+      console.log(this.userInfo);
+      this.editUserForm.patchValue({
+        firstName: this.userInfo?.firstName,
+        lastName: this.userInfo?.lastName,
+        subtitle: this.userInfo?.subtitle,
+        about: this.userInfo?.about,
+      });
     });
   }
   removeWhiteSpaces(event: Event) {
@@ -64,7 +81,21 @@ export class EditUserComponent {
   dragOver(event: Event) {
     event.preventDefault();
   }
-  onSubmit(): void {}
+  onSubmit(): void {
+    if (this.editUserForm.invalid) {
+      return;
+    }
+
+    const updatedUser = {
+      ...this.userInfo,
+      firstName: this.editUserForm.get('firstName')!.value,
+      lastName: this.editUserForm.get('lastName')!.value,
+      subtitle: this.editUserForm.get('subtitle')!.value ?? null,
+      about: this.editUserForm.get('about')!.value ?? null,
+      profileImage: this.uploadedImage,
+    };
+    console.log(updatedUser);
+  }
   onCancel(): void {
     this.formSubmitted.emit(null);
     this.clearForm();
