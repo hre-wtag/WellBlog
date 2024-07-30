0986e85af072e2dd52a947b0f586d57cf9d56e34
Helena Aktar Reya <helena.reya@welldev.io>
2024-07-30 13:18:39 +0600

    added edit btn if the blog is logged in users

--- src/app/core/services/blog.service.ts
+++ src/app/core/services/blog.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Blog } from '../interfaces/blog';
+import { AuthService } from './auth.service';
+import { User } from '../interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlogService {
-blogs$=new BehaviorSubject<Blog[]|null>(null);
-  constructor() { }
+  blogs$ = new BehaviorSubject<Blog[] | null>(null);
+
+  private authService = inject(AuthService);
+
+  isMyBlog(bloggerId: number): boolean {
+    let isMyBlog = false;
+    const userSub = this.authService.user$.subscribe((user: User | null) => {
+      isMyBlog = user?.id === bloggerId;
+    });
+    userSub.unsubscribe();
+    return isMyBlog;
+  }
 }
