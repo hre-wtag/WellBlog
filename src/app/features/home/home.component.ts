ca749feff134012a25902bd2691a6d9317198f25
Helena Aktar Reya <helena.reya@welldev.io>
2024-08-08 18:32:13 +0600

    took margin end on  gloabl style

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BlogService } from '../../core/services/blog.service';
+import { bufferCount, map, Observable } from 'rxjs';
+import { Blog } from '../../core/interfaces/blog';
+import { BlogCardComponent } from '../blog-card/blog-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BlogCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private blogService = inject(BlogService);
+  blogGroups: Blog[][] | null = null;
+  heroBlog: Blog | null = null;
+  ngOnInit(): void {
+    this.blogService.blogs$.subscribe((blogs) => {
+      this.blogGroups = this.groupBlogs(blogs) ?? null;
+    });
+    this.heroBlog = this.blogGroups ? this.blogGroups[0][0] : null;
+    console.log(this.blogGroups, 'blogs');
+  }
+
+  groupBlogs(blogs: Blog[] | null): Blog[][] {
+    const groupedBlogs: Blog[][] = [];
+    if (blogs) {
+      for (let i = 0; i < blogs.length; i += 3) {
+        groupedBlogs.push(blogs.slice(i, i + 3));
+      }
+    }
+    return groupedBlogs;
+  }
+}
