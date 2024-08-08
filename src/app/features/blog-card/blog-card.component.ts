2797bbc4858a5b7818ee027365c173154381afd9
Helena Aktar Reya <helena.reya@welldev.io>
2024-08-08 20:17:45 +0600

    added delete btn only on profile page

--- src/app/features/blog-card/blog-card.component.ts
+++ src/app/features/blog-card/blog-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnChanges } from '@angular/core';
 import { Blog } from '../../core/interfaces/blog';
 import {
   BLOG_ROUTE,
   DEFAULT_PROFILE_PHOTO_SRC,
+  PROFILE_ROUTE,
   SLASH,
 } from '../../core/utils/constants';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-card',
@@ -15,10 +16,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './blog-card.component.html',
   styleUrl: './blog-card.component.scss',
 })
-export class BlogCardComponent {
+export class BlogCardComponent implements OnChanges {
   @Input() blog!: Blog;
   default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
-  blog_route = SLASH + BLOG_ROUTE;
+  blog_route: string = SLASH + BLOG_ROUTE;
+  showDeleteBtn: boolean = false;
+  profile_route: string = SLASH + PROFILE_ROUTE;
+  private router = inject(Router);
+  ngOnChanges(): void {
+    this.showDeleteBtn = this.router.url === this.profile_route ? true : false;
+  }
   onDelete(id: number): void {
     console.log(id);
   }
