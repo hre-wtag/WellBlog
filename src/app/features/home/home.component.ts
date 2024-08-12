2a037b324c7d222d6551a88be07a3579557b4ba9
Helena Aktar Reya <helena.reya@welldev.io>
2024-08-12 13:28:20 +0600

    updated hero blog details based on logged in status

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { BlogService } from '../../core/services/blog.service';
 import { bufferCount, map, Observable } from 'rxjs';
 import { Blog } from '../../core/interfaces/blog';
@@ -6,6 +6,8 @@ import { BlogCardComponent } from '../blog-card/blog-card.component';
 import { CommonModule } from '@angular/common';
 import { TooltipDirective } from '../../shared/Directives/tooltip.directive';
 import { DEFAULT_PROFILE_PHOTO_SRC } from '../../core/utils/constants';
+import { AuthService } from '../../core/services/auth.service';
+import { User } from '../../core/interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -18,13 +20,26 @@ export class HomeComponent implements OnInit {
   private blogService = inject(BlogService);
   blogGroups: Blog[][] | null = null;
   heroBlog: Blog | null = null;
-  default_profile_photo = DEFAULT_PROFILE_PHOTO_SRC;
+  default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
+  private destroyRef = inject(DestroyRef);
+  private authService = inject(AuthService);
+  isLoggedin: boolean = false;
   ngOnInit(): void {
     this.blogService.blogs$.subscribe((blogs) => {
       this.blogGroups = this.groupBlogs(blogs) ?? null;
     });
     this.heroBlog = this.blogGroups ? this.blogGroups[0][0] : null;
     console.log(this.blogGroups, 'blogs');
+    const userSubcription = this.authService.user$.subscribe(
+      (user: User | null) => {
+        if (user) {
+          this.isLoggedin = true;
+        } else {
+          this.isLoggedin = false;
+        }
+      }
+    );
+    this.destroyRef.onDestroy(() => userSubcription.unsubscribe());
   }
 
   groupBlogs(blogs: Blog[] | null): Blog[][] {
