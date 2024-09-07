016fbcbbf5cb10aefe6701666a736d145c13a4aa
reya <creya07@gmail.com>
2024-09-07 10:46:49 +0600

    fixed blog loading issue

--- src/app/features/blog-card/blog-card.component.ts
+++ src/app/features/blog-card/blog-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Blog } from '../../core/interfaces/blog';
 import { DEFAULT_PROFILE_PHOTO_SRC } from '../../core/utils/constants';
 import { CommonModule } from '@angular/common';
@@ -10,15 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './blog-card.component.html',
   styleUrl: './blog-card.component.scss',
 })
-export class BlogCardComponent implements OnInit {
+export class BlogCardComponent implements OnChanges {
   @Input() blog!: Blog;
 
   default_profile_photo: string = DEFAULT_PROFILE_PHOTO_SRC;
   formattedBlogTitle: string = '';
 
-  ngOnInit(): void {
-    if (this.blog ) {
-      this.formattedBlogTitle =  this.blog.title.length > 30 ?this.blog.title.slice(0, 30) + '...': this.blog.title;
+  ngOnChanges(): void {
+    if (this.blog) {
+      this.formattedBlogTitle =
+        this.blog.title.length > 30
+          ? this.blog.title.slice(0, 30) + '...'
+          : this.blog.title;
     }
   }
 }
